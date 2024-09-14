8a242c9a7d801bf78b215c22bdc2270d61d2766b
Orisamika Samuel <satechmedia@gmail.com>
2024-09-14 14:21:18 +0100

    feat:data-type

---
+++ intro/data-type.js
@@ -0,0 +1,52 @@
+// String 'gguf' "jhj" `hjkh`
+let name = "John Doe";
+
+// Number
+let age = 30;
+
+// Boolean
+let isRaining = true;
+
+// Undefined unassigned variable
+let address;
+// console.log(address)
+
+// Null
+let car = null;
+// console.log(car)
+
+// Object
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+
+let color ={
+    white: "#ffffff",
+    black: "000000"
+}
+
+let basket ={
+    car: ["BMW", "Toyota", "Honda", "Benz", "Ford"],
+    color: ["red", "green", "blue"],
+    fruit: ["apple", "orange", "banana", "grape"]
+}
+// console.log(basket)
+// console.log("person:", person, "Colors:", color)
+
+// Array
+let colors = ["red", "green", "blue"];
+let cars = ["BMW", "Toyota", "Honda", "Benz", "Ford"];
+// console.log("cars:", cars)
+
+// Symbol
+let sym = Symbol('foo');
+// console.log(sym)
+
+// BigInt
+let x = BigInt(123456789012345);
+let y = BigInt(7777777)
+
+
+let bigNumber = BigInt(1234567890123456789012345678901234567890);
